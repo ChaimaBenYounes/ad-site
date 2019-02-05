e475597aa80d097be2d1eec26c869e9a2607695c
chaimaby <chaimabenyounes1@gmail.com>
2019-02-05 16:46:15 +0100

    add user.firstName in twig account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 /**
  * @IsGranted("ROLE_ADMIN")
@@ -14,8 +15,9 @@ class AccountController extends AbstractController
     /**
      * @Route("/account", name="app_account")
      */
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+        $logger->debug('Checking account page for '.$this->getUser()->getEmail());
         return $this->render('account/index.html.twig');
     }
 }
--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -5,14 +5,10 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-
 use App\Entity\{Advert, Image, Application, Skill, AdvertSkill, Category};
 use App\Repository\AdvertRepository;
 use App\Form\{AdvertType, AdvertEditType, SkillType, AdvertSkillType };
